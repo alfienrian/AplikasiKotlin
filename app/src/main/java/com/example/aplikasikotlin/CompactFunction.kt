e685682f6d721431fdd1db0057ae92bb95158866
Amro Alfien <55046247+alfienrian@users.noreply.github.com>
2024-03-01 23:00:37 +0700

    Codelab 2: Latihan Compact Function

---
+++ app/src/main/java/com/example/aplikasikotlin/CompactFunction.kt
@@ -0,0 +1,57 @@
+package com.example.aplikasikotlin
+
+import java.util.*    // required import
+
+fun main() {
+    swim()  // menggunakan argumen speed default
+    swim("slow")  // argument speed dengan value "slow"
+    swim(speed="turtle-like")  // argument speed dengan nama argument dan value
+    feedTheFish()  // panggil fungsi feedTheFish
+}
+
+// Compact function
+fun isTooHot(temperature: Int) = temperature > 30
+fun isDirty(dirty: Int) = dirty > 30
+fun isSunday(day: String) = day == "Sunday"
+
+// Normal function
+fun swim(speed:String = "fast") {
+    println("swimming $speed")
+}
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) ->  true
+        else -> false
+    }
+}
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun fishFood (day : String) : String {
+    var food = ""
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
+    }
+    return food
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
+}
+

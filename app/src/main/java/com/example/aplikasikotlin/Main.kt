c484340932746894035860934bcf3e56d3acab5b
Amro Alfien <55046247+alfienrian@users.noreply.github.com>
2024-03-15 23:23:17 +0700

    Codelab 3 (step 5): Membuat subclass dan inheritance

--- app/src/main/java/com/example/aplikasikotlin/Main.kt
+++ app/src/main/java/com/example/aplikasikotlin/Main.kt
@@ -1,34 +1,44 @@
 package com.example.aplikasikotlin
 
 import com.example.aplikasikotlin.model.Aquarium
+import com.example.aplikasikotlin.model.TowerTank
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
     myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
-
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
-
-    // default height and length
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-
-    // default width
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-
-    // everything custom
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
-
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
-
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
+
+
+// ------ Arsip Codelab 3 latihan sebelumnya ------
+//fun buildAquarium() {
+//    val myAquarium = Aquarium()
+//    myAquarium.printSize()
+//    myAquarium.height = 60
+//    myAquarium.printSize()
+//
+//    val aquarium1 = Aquarium()
+//    aquarium1.printSize()
+//
+//    // default height and length
+//    val aquarium2 = Aquarium(width = 25)
+//    aquarium2.printSize()
+//
+//    // default width
+//    val aquarium3 = Aquarium(height = 35, length = 110)
+//    aquarium3.printSize()
+//
+//    // everything custom
+//    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+//    aquarium4.printSize()
+//
+//    val aquarium6 = Aquarium(numberOfFish = 29)
+//    aquarium6.printSize()
+//    aquarium6.volume = 70
+//    aquarium6.printSize()
+//
+//}
 
 fun main() {
     buildAquarium()
--- app/src/main/java/com/example/aplikasikotlin/model/Aquarium.kt
+++ app/src/main/java/com/example/aplikasikotlin/model/Aquarium.kt
@@ -1,6 +1,9 @@
 package com.example.aplikasikotlin.model
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import java.lang.Math.PI
+
+// Codelab 3 (step 5): Membuat subclass dan inheritance
+open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
     constructor(numberOfFish: Int) : this() {
         // 2,000 cm^3 per fish + extra room so water doesn't spill
         val tank = numberOfFish * 2000 * 1.1
@@ -16,24 +19,42 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
     * */
     // 1 liter = 1000 cm^3
     // Codelab 3 (step 4): Mengatur visibilitas class member
-    var volume: Int
-        get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
-        private set(value) {
+    open var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
             height = (value * 1000) / (width * length)
         }
+
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
+
 
     init {
         println("aquarium initializing")
     }
 
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
-
-        // 1 liter = 1000 cm^3
-        println("Volume: $volume liters")
-        println("---------------------------\n") // Garis batas
+        // 1 l = 1000 cm^3
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
     }
+}
+
+// Subclass dari class Aquarium
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+
+    override var water = volume * 0.8
+    override val shape = "cylinder"
 
 }
